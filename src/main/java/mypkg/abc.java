2f7aae02cf16d9b62f7ef4fcba0997cb6b9c18eb
Gaurav Bhandari <gauravbhandari1954@gmail.com>
2023-02-21 10:52:48 +0530

    first commit

---
+++ src/main/java/mypkg/abc.java
@@ -0,0 +1,10 @@
+package mypkg;
+public class abc {
+	public static void show() {
+		System.out.println("this is function from the jar file");
+	}
+	public static void main(String args[]) {
+		
+	}
+
+}
